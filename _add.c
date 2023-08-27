da8df8c23268752178563c0080ef97b2da19c932
Eman288 <emantamam107@gmail.com>
2023-08-26 20:26:03 -0700

    missing a new line :)

--- _add.c
+++ _add.c
@@ -13,7 +13,7 @@ void add(stack_t **head, unsigned int line_num)
 	temp = *head;
 	if (temp == NULL || temp->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short", line_num);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_num);
 		fclose(d.fp);
 		free(d.values);
 		_free(head);
